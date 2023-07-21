4e4fe7bd4f61c561893d6f24938de2afb93880cb
Cristina-Irina <cristina.rmackay@gmail.com>
2023-07-21 17:54:52 +0300

    update

--- src/library.js
+++
@@ -1,5 +0,0 @@
-// FT-11 Creează containerul paginii "My Library" pentru poziționarea corectă a cardurilor de film (mobile, tablet, desktop)
-// FT-12 Creează header-ul pentru pagina "My Library" (mobile, tablet, desktop)
-// FT-13 La click pe logo, se afișează pagina principală
-// FT-14 La click pe butonul "Watched", se afișează filmele vizionate de utilizator
-// FT-15 La click pe butonul "Queue", se afișează filmele adăugate în coada de așteptare a utilizatorului
